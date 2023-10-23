2101b45c8af19615b5114f2d1a41bed5fe40e37d
Yoshiy0sh <yoshire032@gmail.com>
2023-10-23 18:52:49 +0200

    création Humain et histoire 1

---
+++ PierreSabre/src/personnages/Humain.java
@@ -0,0 +1,54 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonFavorite;
+	private int argent;
+	
+	public Humain(String nom,String boissonFavorite,int argent) {
+		this.nom = nom;
+		this.boissonFavorite = boissonFavorite;
+		this.argent = argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getBoissonFavorite() {
+		return boissonFavorite;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + getNom() + " et j'aime boire du " + getBoissonFavorite());
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de "+getBoissonFavorite()+" ! GLOUPS !");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (getArgent() >= prix) {
+			parler("J'ai "+getArgent()+" sous en poche. Je vais pouvoir acheter "+ bien+ " à "+ prix + " sous");
+			perdreArgent(prix);
+		} else {
+			parler("J'ai "+getArgent()+" sous en poche. Je ne peux même pas m'offrir "+ bien+ " à "+ prix + " sous");
+		}
+	}
+	
+	public void gagnerArgent(int gain){
+		this.argent += gain;
+	}
+	
+	public void perdreArgent(int perte) {
+		this.argent -= perte;
+	}
+}

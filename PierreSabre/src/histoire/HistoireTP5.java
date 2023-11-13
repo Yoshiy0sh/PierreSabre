d9d7595f84ff01b11ee8aa3001acbcb708290c0b
Yoshiy0sh <yoshire032@gmail.com>
2023-11-13 17:47:43 +0100

    TP5 jusqu'a 1.c

--- PierreSabre/src/histoire/HistoireTP5.java
+++ PierreSabre/src/histoire/HistoireTP5.java
@@ -1,22 +1,27 @@
 package histoire;
 
+import personnages.Commercant;
 import personnages.Ronin;
 import personnages.Yakuza;
-import personnages.Commercant;
 
-public class HistoireTP4 {
+public class HistoireTP5 {
 	
 	public static void main(String[] args) {
 		Commercant marco = new Commercant("Marco", 20);
 		Commercant chonin = new Commercant("Chonin", 40);
 		Commercant kumi = new Commercant("Kumi", 10);
-		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", "Warsong",30);
+		Yakuza yaku = new Yakuza("Warsong","Yaku Le Noir", "whisky",30);
 		Ronin roro = new Ronin("Roro", "shochu", 60);
 		marco.faireConnaissanceAvec(roro);
 		marco.faireConnaissanceAvec(yaku);
 		marco.faireConnaissanceAvec(chonin);
 		marco.faireConnaissanceAvec(kumi);
+//		yaku.faireConnaissanceAvec(marco);
+//		roro.faireConnaissanceAvec(marco);
+		marco.listerConnaissance();
+//		roro.listerConnaissance();
+//		yaku.listerConnaissance();
 		
-
 	}
+
 }
--- PierreSabre/src/personnages/Humain.java
+++ PierreSabre/src/personnages/Humain.java
@@ -4,6 +4,8 @@ public class Humain {
 	protected String nom;
 	protected String boissonFavorite;
 	protected int argent;
+	protected int nbConnaissance = 0;
+	protected Humain[] memoire = new Humain[3];
 	
 	public Humain(String nom,String boissonFavorite,int argent) {
 		this.nom = nom;
@@ -54,5 +56,44 @@ public class Humain {
 	
 	public void perdreArgent(int perte) {
 		this.argent -= perte;
+	}
+	
+	public int getNbConnaissance() {
+		return nbConnaissance;
+	}
+	
+	private void memoriser(Humain Homme) {
+		int curseur = getNbConnaissance()%3;
+		memoire[curseur] = Homme;
+	}
+	
+	public void setNbConnaissance(int nbConnaissance) {
+		this.nbConnaissance = nbConnaissance;
+	}
+	
+	public void faireConnaissanceAvec(Humain autreHumain) {
+		direBonjour();
+		autreHumain.direBonjour();
+		autreHumain.memoriser(this);
+		memoriser(autreHumain);
+		setNbConnaissance(getNbConnaissance()+1);
+		
+	}
+	
+	public void listerConnaissance() {
+		int connaissance = getNbConnaissance();
+		String texte = "je connais beaucoup de monde dont ";
+		if(connaissance >= 3) {
+			for(int i= 0;i<3;i++) {
+				texte += memoire[(connaissance - ((connaissance /3)*3) + i)%3].getNom()+ " ";
+				
+			}
+		}
+		else {
+			for(int i = 0;i<connaissance;i++) {
+				texte += memoire[i].getNom() + " ";
+			}
+			parler(texte);
+		}
 	}
 }
